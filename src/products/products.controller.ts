b404e1865e668a395f38fb72278c3c64683e90bc
Ganga <ganga.bhavani@innodeed.com>
2023-10-31 17:43:26 +0530

    nest js code added

---
+++ src/products/products.controller.ts
@@ -0,0 +1,45 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Delete,
+  Param,
+  ParseIntPipe,
+  Patch,
+} from '@nestjs/common';
+import { Product } from './product.entity';
+import { ProductsService } from './products.service';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private productsService: ProductsService) {}
+
+  @Get()
+  async GetAll(): Promise<Product[]> {
+    return this.productsService.getAll();
+  }
+
+  @Get(':id')
+  async GetOne(@Param('id', ParseIntPipe) id: number): Promise<Product> {
+    return this.productsService.getOneById(id);
+  }
+
+  @Post()
+  async create(@Body() product: any): Promise<Product> {
+    return this.productsService.create(product);
+  }
+
+  @Patch(':id')
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() product: any,
+  ): Promise<Product> {
+    return this.productsService.update(id, product);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: number): Promise<number> {
+    return this.productsService.delete(id);
+  }
+}
